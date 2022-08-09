16b39b460f82173015d527c1b9fb90ec01775480
Helen Li <109709284+hellyjellyfish@users.noreply.github.com>
2022-08-09 18:20:20 -0400

    Update SQL Rank.sql

--- SQL Rank.sql
+++ SQL Rank.sql
@@ -1,9 +1,5 @@
 
 /*
-You are working as a recruiter at a big IT company, and you're actively looking for candidates who take the top places in major programming contests. Since the grand finale of the annual City Competition, you've been reaching out to the top participants from the leaderboard, and successfully so.
-
-You have already interviewed all the prize winners (the top 3 participants), but that's not enough right now. Your company needs more specialists, so now you would like to connect with the participants who took the next 5 places.
-
 The contest leaderboard is stored in a table leaderboard with the following columns:
 
 id: unique id of the participant;
